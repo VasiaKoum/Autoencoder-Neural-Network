aa59ec4aa7cee07d80e2ffe01df40988a1851755
VasiaKoum <vasiakoum@hotmail.com>
2020-11-09 01:14:30 +0200

    changes

--- autoencoder.py
+++ autoencoder.py
@@ -2,6 +2,8 @@
 import sys
 import time
 import struct
+import numpy as np
+import matplotlib.pyplot as plt
 from functions import *
 
 def main():
@@ -16,20 +18,18 @@ def main():
         images = int.from_bytes(file.read(4), byteorder='big')
         rows = int.from_bytes(file.read(4), byteorder='big')
         columns = int.from_bytes(file.read(4), byteorder='big')
-        pixels = list(bytes_group(rows*columns, file.read(), fillvalue=0))
-
-        print(magic_num, images, rows, columns)
-        # print(pixels[-1])
+        print("Begin autoencoder with:    (magic_num)->"+str(magic_num)+"    (images)->"+str(images)+"    (rows)->"+str(rows)+"    (cols)->"+str(columns))
+        print("Store data in array...")
+        # 2d numpy array for images->pixels
+        pixels = np.array(list(bytes_group(rows*columns, file.read(), fillvalue=0)))
 
         # User choices:
         # run_again = 1
         # while(run_again>0 and run_again<4):
         #     try:
-        #         run_again = int(input("Choose one from below options(1-4): \n1)Execute program with different parameters\n2)Show error-graphs\n3)Save the existing model\n4)Exit\n\n"))
+        #         run_again = int(input("\nChoose one from below options(1-4): \n1)Execute program with different parameters\n2)Show error-graphs\n3)Save the existing model\n4)Exit\n\n"))
         #     except:
         #         print("Invalid choice.Try again\n")
-
-
 start_time = time.time()
 main()
 print("\nExecution time: %s seconds" % (time.time() - start_time))
--- functions.py
+++ functions.py
@@ -2,3 +2,9 @@ from itertools import zip_longest
 
 def bytes_group(n, iterable, fillvalue=None):
     return zip_longest(*[iter(iterable)]*n, fillvalue=fillvalue)
+
+def encoder(input_img):
+    print("encoder")
+
+def decoder(enc_filder):
+    print("decoder")
