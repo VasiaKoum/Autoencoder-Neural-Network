dad0b8076641d58528402ad042c1e4cfa4bbe909
VasiaKoum <vasiakoum@hotmail.com>
2020-11-13 03:00:21 +0200

    update encoder & decoder

--- autoencoder.py
+++ autoencoder.py
@@ -3,7 +3,10 @@ import sys
 import time
 import struct
 import numpy as np
+from keras.models import Model
+from keras.optimizers import RMSprop
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 from functions import *
 
 def main():
@@ -22,6 +25,14 @@ def main():
         print("Store data in array...")
         # 2d numpy array for images->pixels
         pixels = np.array(list(bytes_group(rows*columns, file.read(), fillvalue=0)))
+        print("Data ready in numpy array! Begin building model...")
+
+        input_img = Input(shape=(28, 28, 1))
+        # input_img = np.resize(pixels,(28,28,1))
+        autoencoder = Model(input_img, decoder(encoder(input_img)))
+        autoencoder.compile(loss='mean_squared_error', optimizer=RMSprop())
+        train_X,valid_X,train_ground,valid_ground = train_test_split(pixels, pixels, test_size=0.2, random_state=13)
+        # autoencoder_train = autoencoder.fit(train_X, train_ground, batch_size=128,epochs=5,verbose=1,validation_data=(valid_X, valid_ground))
 
         # User choices:
         # run_again = 1
--- functions.py
+++ functions.py
@@ -1,10 +1,33 @@
 from itertools import zip_longest
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, BatchNormalization
 
 def bytes_group(n, iterable, fillvalue=None):
     return zip_longest(*[iter(iterable)]*n, fillvalue=fillvalue)
 
 def encoder(input_img):
-    print("encoder")
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img) #28 x 28 x 32
+    conv1 = BatchNormalization()(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) #14 x 14 x 32
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1) #14 x 14 x 64
+    conv2 = BatchNormalization()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #7 x 7 x 64
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2) #7 x 7 x 128 (small & thick)
+    conv3 = BatchNormalization()(conv3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv3) #7 x 7 x 256 (small & thick)
+    conv4 = BatchNormalization()(conv4)
+    return conv4
 
-def decoder(enc_filder):
-    print("decoder")
+def decoder(convenc):
+    #decoder
+    conv5 = Conv2D(256, (3, 3), activation='relu', padding='same')(convenc) #7 x 7 x 256
+    conv5 = BatchNormalization()(conv5)
+    conv6 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv5) #7 x 7 x 128
+    conv6 = BatchNormalization()(conv6)
+    conv7 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv5) #7 x 7 x 64
+    conv7 = BatchNormalization()(conv7)
+    up1 = UpSampling2D((2,2))(conv7) #14 x 14 x 64
+    conv8 = Conv2D(32, (3, 3), activation='relu', padding='same')(up1) # 14 x 14 x 32
+    conv8 = BatchNormalization()(conv8)
+    up2 = UpSampling2D((2,2))(conv8) # 28 x 28 x 32
+    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(up2) # 28 x 28 x 1
+    return decoded
