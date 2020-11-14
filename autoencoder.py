4092c1fede4ca802518490c933ac166d72999567
VasiaKoum <vasiakoum@hotmail.com>
2020-11-14 16:38:06 +0200

    changes

--- autoencoder.py
+++ autoencoder.py
@@ -6,9 +6,9 @@ import numpy as np
 from keras.models import Model
 from keras.optimizers import RMSprop
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from functions import *
 
+# A part hyperparameters -> number of layers, filter size, number of filters/layer, number of epochs, batch size
 def main():
     if(len(sys.argv) != 3):
         sys.exit("Wrong or missing parameter. Please execute with: -d dataset")
@@ -16,31 +16,28 @@ def main():
         sys.exit("Wrong or missing parameter. Please execute with: -d dataset")
     dataset = sys.argv[2]
 
-    with open(dataset, "rb") as file:
-        magic_num = int.from_bytes(file.read(4), byteorder='big')
-        images = int.from_bytes(file.read(4), byteorder='big')
-        rows = int.from_bytes(file.read(4), byteorder='big')
-        columns = int.from_bytes(file.read(4), byteorder='big')
-        print("Begin autoencoder with:    (magic_num)->"+str(magic_num)+"    (images)->"+str(images)+"    (rows)->"+str(rows)+"    (cols)->"+str(columns))
-        print("Store data in array...")
-        # 2d numpy array for images->pixels
-        pixels = np.array(list(bytes_group(rows*columns, file.read(), fillvalue=0)))
-        print("Data ready in numpy array! Begin building model...")
+    # numarray[0] -> magic_number, [1] -> images, [2] -> rows, [3] -> columns
+    pixels, numarray = numpy_from_dataset(dataset, 4)
+    if(len(numarray)!=4 or len(pixels)==0):
+        sys.exit("Input dataset does not have the required number of values")
+    train_X, valid_X, train_Y, valid_Y = reshape_dataset(pixels, numarray)
+    print("Data ready in numpy array!")
 
-        input_img = Input(shape=(28, 28, 1))
-        # input_img = np.resize(pixels,(28,28,1))
+    while(True):
+        print("Begin building model...")
+        input_img = Input(shape=(numarray[2], numarray[3], 1))
         autoencoder = Model(input_img, decoder(encoder(input_img)))
         autoencoder.compile(loss='mean_squared_error', optimizer=RMSprop())
-        train_X,valid_X,train_ground,valid_ground = train_test_split(pixels, pixels, test_size=0.2, random_state=13)
-        # autoencoder_train = autoencoder.fit(train_X, train_ground, batch_size=128,epochs=5,verbose=1,validation_data=(valid_X, valid_ground))
+        # autoencoder_train = autoencoder.fit(train_X, train_Y, batch_size=32, epochs=5,verbose=1,validation_data=(valid_X, valid_Y))
+
+        # autoencoder = load_model("autoencoder")
+        # autoencoder.compile(loss='mean_squared_error', optimizer=RMSprop())
 
         # User choices:
-        # run_again = 1
-        # while(run_again>0 and run_again<4):
-        #     try:
-        #         run_again = int(input("\nChoose one from below options(1-4): \n1)Execute program with different parameters\n2)Show error-graphs\n3)Save the existing model\n4)Exit\n\n"))
-        #     except:
-        #         print("Invalid choice.Try again\n")
+        parameters, continue_flag = user_choices(autoencoder, "autoencoder")
+        if(not continue_flag):
+            break;
+
 start_time = time.time()
 main()
 print("\nExecution time: %s seconds" % (time.time() - start_time))
--- functions.py
+++ functions.py
@@ -1,5 +1,24 @@
+import numpy as np
 from itertools import zip_longest
+from sklearn.model_selection import train_test_split
+from keras.models import Model, model_from_json
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, BatchNormalization
+
+def numpy_from_dataset(inputpath, numbers):
+    pixels = []
+    numarray = []
+    with open(inputpath, "rb") as file:
+        for x in range(numbers):
+            numarray.append(int.from_bytes(file.read(4), byteorder='big'))
+        # print("Begin autoencoder with: "+str())
+        print("Storing data in array...")
+        # 2d numpy array for images->pixels
+        if numbers == 4:
+            pixels = np.array(list(bytes_group(numarray[2]*numarray[3], file.read(), fillvalue=0)))
+        elif numbers == 2:
+            # pixels = np.array(list(bytes_group(rows*columns, file.read(), fillvalue=0)))
+            print(numarray[0], " ", numarray[1])
+    return pixels, numarray
 
 def bytes_group(n, iterable, fillvalue=None):
     return zip_longest(*[iter(iterable)]*n, fillvalue=fillvalue)
@@ -31,3 +50,63 @@ def decoder(convenc):
     up2 = UpSampling2D((2,2))(conv8) # 28 x 28 x 32
     decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(up2) # 28 x 28 x 1
     return decoded
+
+def save_model(model, modelname):
+    print("Saving Model: "+modelname+".json & "+modelname+".h5...")
+    # Save model in JSON file
+    model_json = model.to_json()
+    with open(modelname+".json", "w") as json_file:
+        json_file.write(model_json)
+    # Save weights from model in h5 file
+    model.save_weights(modelname+".h5")
+
+def load_model(modelname):
+    # Load model from JSON file
+    print("Loading Model: "+modelname+".json & "+modelname+".h5...")
+    json_file = open(modelname+".json", 'r')
+    autoencoder_json = json_file.read()
+    json_file.close()
+    autoencoder = model_from_json(autoencoder_json)
+    # Load weights from h5 file
+    autoencoder.load_weights(modelname+".h5")
+    return autoencoder
+
+def error_graphs():
+    print("Here error_graphs")
+
+def reshape_dataset(dataset, numarray):
+    train_X, valid_X, train_Y, valid_Y = train_test_split(dataset, dataset, test_size=0.2, random_state=13)
+    # Reshapes to (x, rows, columns)
+    train_X = np.reshape(train_X, (-1, numarray[2], numarray[3]))
+    valid_X = np.reshape(valid_X, (-1, numarray[2], numarray[3]))
+    train_Y = np.reshape(train_Y, (-1, numarray[2], numarray[3]))
+    valid_Y = np.reshape(valid_Y, (-1, numarray[2], numarray[3]))
+    return train_X, valid_X, train_Y, valid_Y
+
+def user_choices(model, modelname):
+    parameters = []
+    continue_flag = True
+    while(True):
+        try:
+            parameter.clear()
+            run_again = int(input("\nUSER CHOICES: choose one from below options(1-4): \n1)Execute program with different hyperparameters\n2)Show error-graphs\n3)Save the existing model\n4)Exit\n---------------> "))
+            if(run_again==1):
+                parameters.append(int(input("Type number of layers: ")))
+                parameters.append(int(input("Type filter size: ")))
+                parameters.append(int(input("Type number of filters/layer: ")))
+                parameters.append(int(input("Type number of epochs: ")))
+                parameters.append(int(input("Type batch size: ")))
+                break;
+            elif(run_again == 2):
+                error_graphs()
+            elif(run_again == 3):
+                save_model(model, modelname)
+            elif(run_again == 4):
+                continue_flag = False
+                print("Program terminates...\n")
+                break;
+            else:
+                print("Invalid choice.Try again\n")
+        except:
+            print("Invalid choice.Try again\n")
+    return parameters, continue_flag;
