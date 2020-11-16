6e27271cf10654bbfede85b6ac17af70fcfe58e4
VasiaKoum <vasiakoum@hotmail.com>
2020-11-17 01:09:08 +0200

    update encoder & decoder

--- autoencoder.py
+++ autoencoder.py
@@ -8,7 +8,7 @@ from keras.optimizers import RMSprop
 import matplotlib.pyplot as plt
 from functions import *
 
-# A part hyperparameters -> number of layers, filter size, number of filters/layer, number of epochs, batch size
+# Autoencoder hyperparameters -> number of layers, filter size, number of filters/layer, number of epochs, batch size
 def main():
     if(len(sys.argv) != 3):
         sys.exit("Wrong or missing parameter. Please execute with: -d dataset")
@@ -22,20 +22,24 @@ def main():
         sys.exit("Input dataset does not have the required number of values")
     train_X, valid_X, train_Y, valid_Y = reshape_dataset(pixels, numarray)
     print("Data ready in numpy array!")
+    parameters = [4, 3, 8, 5, 100] # Layers,Filter_size,Filters_start,Epochs,Batch_size
 
     while(True):
         print("Begin building model...")
         input_img = Input(shape=(numarray[2], numarray[3], 1))
-        autoencoder = Model(input_img, decoder(encoder(input_img, 4, 32), 4, 32))
+        autoencoder = Model(input_img, decoder(encoder(input_img, parameters), parameters))
         autoencoder.compile(loss='mean_squared_error', optimizer=RMSprop())
-        autoencoder_train = autoencoder.fit(train_X, train_Y, batch_size=124, epochs=5, verbose=1, validation_data=(valid_X, valid_Y))
-        # train_ER = autoencoder.evaluate(train_X, train_Y, verbose=1)
+        train_time = time.time()
+        autoencoder_train = autoencoder.fit(train_X, train_Y, batch_size=parameters[4], epochs=parameters[3], verbose=1, validation_data=(valid_X, valid_Y))
+        train_time = time.time() - train_time
+        train_ER = autoencoder.evaluate(train_X, train_Y, verbose=1)
+        print(train_ER)
 
         # autoencoder = load_model("autoencoder")
         # autoencoder.compile(loss='mean_squared_error', optimizer=RMSprop())
 
         # User choices:
-        parameters, continue_flag = user_choices(autoencoder, "autoencoder", autoencoder_train)
+        parameters, continue_flag = user_choices(autoencoder, "autoencoder", autoencoder_train, parameters, train_time)
         if(not continue_flag):
             break;
 
--- functions.py
+++ functions.py
@@ -11,7 +11,6 @@ def numpy_from_dataset(inputpath, numbers):
     with open(inputpath, "rb") as file:
         for x in range(numbers):
             numarray.append(int.from_bytes(file.read(4), byteorder='big'))
-        # print("Begin autoencoder with: "+str())
         print("Storing data in array...")
         # 2d numpy array for images->pixels
         if numbers == 4:
@@ -24,53 +23,32 @@ def numpy_from_dataset(inputpath, numbers):
 def bytes_group(n, iterable, fillvalue=None):
     return zip_longest(*[iter(iterable)]*n, fillvalue=fillvalue)
 
-def encoder(input_img, layers, filters):
-    mask_size = 3
+def encoder(input_img, parameters):
+    layers = parameters[0]
+    filter_size = parameters[1]
+    filters = parameters[2]
     conv = input_img
-    # for i in range(layers):
-    #     conv = Conv2D(filters, (mask_size, mask_size), activation='relu', padding='same')(conv)
-    #     conv = BatchNormalization()(conv)
-    #     conv = MaxPooling2D(pool_size=(2,2))(conv)
-    #     print("Encoder: ", i, filters)
-    #     filters*=2
-    # return conv
+    for i in range(layers):
+        conv = Conv2D(filters, (filter_size, filter_size), activation='relu', padding='same')(conv)
+        conv = BatchNormalization()(conv)
+        if (i<2):
+            conv = MaxPooling2D(pool_size=(2,2))(conv)
+        filters*=2
+    return conv
 
-    conv1 = Conv2D(8, (3, 3), activation='relu', padding='same')(input_img) #28 x 28 x 8
-    conv1 = BatchNormalization()(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)                           #14 x 14 x 8
-    conv2 = Conv2D(16, (3, 3), activation='relu', padding='same')(pool1)    #14 x 14 x 16
-    conv2 = BatchNormalization()(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)                           #7 x 7 x 16
-    conv3 = Conv2D(32, (3, 3), activation='relu', padding='same')(pool2)    #7 x 7 x 32
-    conv3 = BatchNormalization()(conv3)
-    conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv3)    #7 x 7 x 64
-    conv4 = BatchNormalization()(conv4)
-    return conv4
-
-def decoder(conv, layers, filters):
-    mask_size = 3
-    filters = filters*pow(2,layers-1)
-    # for i in range(layers):
-    #     conv = Conv2D(filters, (mask_size, mask_size), activation='relu', padding='same')(conv)
-    #     conv = BatchNormalization()(conv)
-    #     conv = UpSampling2D((2,2))(conv)
-    #     print("Decoder: ", i, filters)
-    #     filters/=2
-    # conv = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(conv)
-    # return conv
-
-    conv5 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv)     #7 x 7 x 64
-    conv5 = BatchNormalization()(conv5)
-    conv6 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv5)    #7 x 7 x 32
-    conv6 = BatchNormalization()(conv6)
-    conv7 = Conv2D(16, (3, 3), activation='relu', padding='same')(conv5)    #7 x 7 x 16
-    conv7 = BatchNormalization()(conv7)
-    up1 = UpSampling2D((2,2))(conv7)                                        #14 x 14 x 16
-    conv8 = Conv2D(8, (3, 3), activation='relu', padding='same')(up1)       #14 x 14 x 8
-    conv8 = BatchNormalization()(conv8)
-    up2 = UpSampling2D((2,2))(conv8)                                        #28 x 28 x 8
-    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(up2)  #28 x 28 x 1
-    return decoded
+def decoder(conv, parameters):
+    layers = parameters[0]
+    filter_size = parameters[1]
+    filters = parameters[2]*pow(2,parameters[0]-1)
+    for i in range(layers):
+        conv = Conv2D(filters, (filter_size, filter_size), activation='relu', padding='same')(conv)
+        conv = BatchNormalization()(conv)
+        if (i>=layers-2):
+            conv = UpSampling2D((2,2))(conv)
+        filters/=2
+    conv = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(conv)
+    print(conv.shape)
+    return conv
 
 def save_model(model, modelname):
     print("Saving Model: "+modelname+".json & "+modelname+".h5...")
@@ -92,13 +70,18 @@ def load_model(modelname):
     autoencoder.load_weights(modelname+".h5")
     return autoencoder
 
-def error_graphs(modeltrain):
+def error_graphs(modeltrain, parameters, train_time):
     plt.plot(modeltrain.history['loss'], label='train')
     plt.plot(modeltrain.history['val_loss'], label='test')
-    plt.title('Loss / Mean Squared Error')
+    plt.title('Loss / Mean Squared Error in '+str(round(train_time, 3))+'sec')
     plt.ylabel('Loss')
     plt.xlabel('epoch')
+    plt.legend(['loss', 'val_loss'], loc='upper left')
+    graphname = "L"+str(parameters[0])+"_F"+str(parameters[2])+"_E"+str(parameters[3])+"_B"+str(parameters[4])+".png"
+    print("Save graph with name: ",graphname)
+    plt.savefig(graphname)
     plt.show()
+    plt.close()
 
 def reshape_dataset(dataset, numarray):
     train_X, valid_X, train_Y, valid_Y = train_test_split(dataset, dataset, test_size=0.2, random_state=13)
@@ -109,17 +92,16 @@ def reshape_dataset(dataset, numarray):
     valid_Y = np.reshape(valid_Y.astype('float32') / 255., (-1, numarray[2], numarray[3]))
     return train_X, valid_X, train_Y, valid_Y
 
-def user_choices(model, modelname, modeltrain):
-    parameters = []
+def user_choices(model, modelname, modeltrain, parameters, train_time):
     continue_flag = True
     while(True):
         try:
-            parameters.clear()
             run_again = int(input("\nUSER CHOICES: choose one from below options(1-4): \n1)Execute program with different hyperparameters\n2)Show error-graphs\n3)Save the existing model\n4)Exit\n---------------> "))
         except:
             print("Invalid choice.Try again\n")
 
         if(run_again==1):
+            parameters.clear()
             parameters.append(int(input("Type number of layers: ")))
             parameters.append(int(input("Type filter size: ")))
             parameters.append(int(input("Type number of filters/layer: ")))
@@ -127,7 +109,7 @@ def user_choices(model, modelname, modeltrain):
             parameters.append(int(input("Type batch size: ")))
             break;
         elif(run_again == 2):
-            error_graphs(modeltrain)
+            error_graphs(modeltrain, parameters, train_time)
         elif(run_again == 3):
             save_model(model, modelname)
         elif(run_again == 4):
